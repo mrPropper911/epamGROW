5cf0c98a586f241deaab78474b0d81bbe521cc9e
mrPropper911 <vadik123belyahovich@gmail.com>
2021-12-04 15:21:21 +0300

    9-task complite

---
+++ 1-basics-of-software-code-development/9/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        double x1,y1,x2,y2,x3,y3;
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        x1 = Double.parseDouble(buff.readLine());
+        y1 = Double.parseDouble(buff.readLine());
+        x2 = Double.parseDouble(buff.readLine());
+        y2 = Double.parseDouble(buff.readLine());
+        x3 = Double.parseDouble(buff.readLine());
+        y3 = Double.parseDouble(buff.readLine());
+        if ((x3 - x1) / (x2 - x1) == (y3 - y1) / (y2 - y1))
+            System.out.println("Points are located on one straight line");
+        else
+            System.out.println("Points are not located on one straight line");
+    }
+}
